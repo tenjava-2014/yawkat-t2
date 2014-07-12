11b3b57ca2dae92e69b07c394d6e5a6dad5bb489
Jonas Konrad <me@yawk.at>
2014-07-12 14:31:09 +0200

    Add module class

---
+++ src/main/java/com/tenjava/entries/yawkat/t2/module/Module.java
@@ -0,0 +1,24 @@
+package com.tenjava.entries.yawkat.t2.module;
+
+import com.tenjava.entries.yawkat.t2.TenJava;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+
+/**
+ * Represents a part of the behaviour of the plugin.
+ *
+ * @author yawkat
+ */
+public abstract class Module implements Listener {
+    /**
+     * Load and initialize a module by class.
+     */
+    public static void load(Class<? extends Module> moduleClass) {
+        try {
+            Module module = moduleClass.newInstance();
+            TenJava.runTaskOnStartup(() -> Bukkit.getPluginManager().registerEvents(module, TenJava.getInstance()));
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
