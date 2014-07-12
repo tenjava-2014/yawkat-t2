a05e9eecf185f7902ac6e0d09235854058669cd1
Jonas Konrad <me@yawk.at>
2014-07-12 12:43:49 +0200

    Add poweradd command

--- src/main/java/com/tenjava/entries/yawkat/t2/command/Commands.java
+++ src/main/java/com/tenjava/entries/yawkat/t2/command/Commands.java
@@ -1,16 +1,20 @@
 package com.tenjava.entries.yawkat.t2.command;
 
 import com.tenjava.entries.yawkat.t2.TenJava;
+import org.bukkit.ChatColor;
 
 /**
  * @author yawkat
  */
 public class Commands {
+    static final String ERROR_PREFIX = ChatColor.YELLOW.toString() + ChatColor.ITALIC;
+
     private Commands() {}
 
     public static void registerCommands() {
         TenJava.runTaskOnStartup(() -> {
             TenJava.getInstance().getCommand("power").setExecutor(new DisplayPowerCommand());
+            TenJava.getInstance().getCommand("poweradd").setExecutor(new PowerAddCommand());
         });
     }
 }
---
+++ src/main/java/com/tenjava/entries/yawkat/t2/command/PowerAddCommand.java
@@ -0,0 +1,50 @@
+package com.tenjava.entries.yawkat.t2.command;
+
+import com.tenjava.entries.yawkat.t2.Power;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * @author yawkat
+ */
+class PowerAddCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length != 2) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "Usage: /" + command.getAliases().get(0) + " <name> <amount>");
+            return false;
+        }
+
+        String targetName = args[0];
+        Player target = Bukkit.getPlayer(targetName);
+        if (target == null) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "No such player.");
+            return false;
+        }
+        double amount;
+        try {
+            amount = Double.parseDouble(args[1]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "Invalid amount.");
+            return false;
+        }
+
+        if (amount < 0 || !Double.isFinite(amount)) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "Amount must be positive.");
+            return false;
+        }
+
+        Power.addPower(target, amount);
+        sender.sendMessage(ChatColor.GOLD + "Added " +
+                           ChatColor.AQUA + amount +
+                           ChatColor.GOLD + " power for " +
+                           ChatColor.AQUA + target.getName() +
+                           ChatColor.GOLD + ".");
+
+        return true;
+    }
+}
