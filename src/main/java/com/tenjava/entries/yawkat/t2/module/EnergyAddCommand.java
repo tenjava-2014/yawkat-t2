31dfed8c96af0bd9b88d79ba2cccb67d7c11e4a7
Jonas Konrad <me@yawk.at>
2014-07-12 16:39:44 +0200

    Extract main EnergyAddCommand code to EnergyChangeCommand class

--- src/main/java/com/tenjava/entries/yawkat/t2/module/EnergyAddCommand.java
+++ src/main/java/com/tenjava/entries/yawkat/t2/module/EnergyAddCommand.java
@@ -12,52 +12,18 @@ import org.bukkit.entity.Player;
  *
  * @author yawkat
  */
-public class EnergyAddCommand extends CommandModule {
+public class EnergyAddCommand extends EnergyChangeCommand {
     public EnergyAddCommand() {
         super("energyadd");
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // check for args
-        if (args.length != 2) {
-            sender.sendMessage(Commands.ERROR_PREFIX + "Usage: /" + command.getName() + " <name> <amount>");
-            return true;
-        }
-
-        // get the target player
-        String targetName = args[0];
-        @SuppressWarnings("deprecation")
-        Player target = Bukkit.getPlayer(targetName);
-        // not online
-        if (target == null) {
-            sender.sendMessage(Commands.ERROR_PREFIX + "No such player.");
-            return true;
-        }
-        // get the amount
-        double amount;
-        try {
-            amount = Double.parseDouble(args[1]);
-        } catch (NumberFormatException e) {
-            // invalid
-            sender.sendMessage(Commands.ERROR_PREFIX + "Invalid amount.");
-            return true;
-        }
-
-        // check for negative amount, NaN or +-INF (does parseDouble return those?)
-        if (amount < 0 || !Double.isFinite(amount)) {
-            sender.sendMessage(Commands.ERROR_PREFIX + "Amount must be positive.");
-            return true;
-        }
-
-        // perform
+    protected void perform(CommandSender sender, Player target, double amount) {
         Energy.addEnergy(target, amount);
         sender.sendMessage(ChatColor.GOLD + "Added " +
                            ChatColor.AQUA + Commands.toDisplayString(amount) +
                            ChatColor.GOLD + " energy for " +
                            ChatColor.AQUA + target.getName() +
                            ChatColor.GOLD + ".");
-
-        return true;
     }
 }
---
+++ src/main/java/com/tenjava/entries/yawkat/t2/module/EnergyChangeCommand.java
@@ -0,0 +1,55 @@
+package com.tenjava.entries.yawkat.t2.module;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * @author yawkat
+ */
+abstract class EnergyChangeCommand extends CommandModule {
+    public EnergyChangeCommand(String commandName) {
+        super(commandName);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // check for args
+        if (args.length != 2) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "Usage: /" + command.getName() + " <name> <amount>");
+            return true;
+        }
+
+        // get the target player
+        String targetName = args[0];
+        @SuppressWarnings("deprecation")
+        Player target = Bukkit.getPlayer(targetName);
+        // not online
+        if (target == null) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "No such player.");
+            return true;
+        }
+        // get the amount
+        double amount;
+        try {
+            amount = Double.parseDouble(args[1]);
+        } catch (NumberFormatException e) {
+            // invalid
+            sender.sendMessage(Commands.ERROR_PREFIX + "Invalid amount.");
+            return true;
+        }
+
+        // check for negative amount, NaN or +-INF (does parseDouble return those?)
+        if (amount < 0 || !Double.isFinite(amount)) {
+            sender.sendMessage(Commands.ERROR_PREFIX + "Amount must be positive.");
+            return true;
+        }
+
+        perform(sender, target, amount);
+
+        return true;
+    }
+
+    protected abstract void perform(CommandSender sender, Player target, double amount);
+}
