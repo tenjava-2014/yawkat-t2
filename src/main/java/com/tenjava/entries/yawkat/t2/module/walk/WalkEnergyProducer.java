2a61c5b69a1f8d518bb87e4a23c5184ebb7dac57
Jonas Konrad <me@yawk.at>
2014-07-12 14:46:32 +0200

    Make energy gain by walking decline getting closer to 15 total energy

--- src/main/java/com/tenjava/entries/yawkat/t2/module/walk/WalkEnergyProducer.java
+++ src/main/java/com/tenjava/entries/yawkat/t2/module/walk/WalkEnergyProducer.java
@@ -19,6 +19,11 @@ public class WalkEnergyProducer extends Module {
      */
     private static final double DISTANCE_ENERGY_MULTIPLIER = 0.01;
 
+    /**
+     * Maximum energy level we can reach by walking. Energy gain will decline when getting close to this limit.
+     */
+    private static final double EARN_LIMIT = 15;
+
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         if (event instanceof PlayerTeleportEvent) {
@@ -36,6 +41,15 @@ public class WalkEnergyProducer extends Module {
         // horizontal distance
         double dist = Math.sqrt(off.getX() * off.getX() + off.getZ() * off.getZ());
         double gain = dist * DISTANCE_ENERGY_MULTIPLIER;
+        double prevEnergy = Energy.getEnergy(event.getPlayer());
+        // how far the limit is away
+        double remainingEnergyUntilLimit = EARN_LIMIT - prevEnergy;
+        if (remainingEnergyUntilLimit < 0) {
+            // reached limit, don't earn more
+            return;
+        }
+        // less gain when close to limit
+        gain *= remainingEnergyUntilLimit / EARN_LIMIT;
         // increase the players energy level
         Energy.addEnergy(event.getPlayer(), gain);
     }
